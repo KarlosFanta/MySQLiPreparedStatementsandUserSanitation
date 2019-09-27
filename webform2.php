b5dffe550f982e9e2d5232ca62f379343f35e5b9
KarlosFanta <leeuwenpatrol@gmail.com>
2019-09-27 23:49:31 +0200

    Update webform2.php

--- webform2.php
+++ webform2.php
@@ -12,6 +12,7 @@ $CustNo = '';
 $TransDate = '';
 $AmtPaid = '';
 $Notes ='';
+$TMethod ='';
 $CustFN ="John";
 $CustEmail = "test@test.com";
 
@@ -49,6 +50,7 @@ $Notes = str_replace('"', '&quot;', $Notes);  //for mailto: emails.
 $Notes = htmlentities( $Notes, ENT_SUBSTITUTE );
   //and also header: charset=UTF-8"   WORKS LIKE A CHARM 2014
 $Notes = mysqli_real_escape_string($link, $Notes);
+$TMethod = $_POST['TMethod'];
 
 
 echo "<br><br>TransNo: ".$TransNo."<br>";
@@ -58,38 +60,46 @@ echo "AmtPaid: $".$AmtPaid."<br>";
 echo "Notes: ".$Notes."<br>";
 echo "CustFN: ".$CustFN."<br>";
 echo "CustEmail: ".$CustEmail."<br>";
+echo "TMethod: ".$TMethod."<br>";
 
 
   
 $TransNoInt = intval($TransNo);
-$query="insert into transactions2 (TransNo, CustNo, TransDate, AmtPaid, Notes)
-VALUES ( ?,  ?, '?', ?, '?') ";
-$query="insert into transactions2 (TransNo, CustNo, TransDate, AmtPaid, Notes)
-VALUES ( ?,  ?, ?, ?, ?) ";
+$query="insert into transactions2 (TransNo, CustNo, TransDate, AmtPaid, Notes, TMethod)
+VALUES ( ?,  ?, ?, ?, ?, ?) ";
 
 echo '<br>';
 
 if ($stmt = mysqli_prepare($link, $query)) {
 
     /* bind parameters for markers */
- mysqli_stmt_bind_param($stmt, "iisds", $TransNoInt,  $CustNo, $TransDate, $AmtPaid, $Notes); //untested
-//iisds possibly stands for: integer integer string decimal string
+ mysqli_stmt_bind_param($stmt, "iisdss", $TransNoInt,  $CustNo, $TransDate, $AmtPaid, $Notes, $TMethod); //untested
+//iisdss possibly stands for: integer integer string decimal string string
 /* `TransNo` int(11) NOT NULL,
   `CustNo` int(11) DEFAULT NULL,
   `TransDate` date NOT NULL,
   `AmtPaid` float DEFAULT NULL,
   `Notes` varchar(500) DEFAULT NULL,
+  `TMethod` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`TransNo`),
   UNIQUE KEY `TransNo` (`TransNo`)
 */
     /* execute query */
     mysqli_stmt_execute($stmt);
 printf("%d Row inserted.\n", mysqli_stmt_affected_rows($stmt));
-
+if ( mysqli_stmt_affected_rows($stmt) == '1')
+echo "<font size = 4  color = green>insert success! </font><br>";
+else
+echo "<font size = 4  color = red><b><b>insert into transactions2 NOT successful!<br>Maybe this primary key number exists already </b></font><br>$query<br>";
     /* bind result variables */
 // mysqli_stmt_bind_result($stmt, $TransNoInt,  $CustNo, '$TransDate', $AmtPaid, '$Notes');
 ///mysqli_stmt_bind_result($stmt);  only for select statement presuamably?
 
+
+
+if (!mysqli_query($link, "SET @a:='this will not work'")) {
+        printf("Error: %s\n", mysqli_error($link));
+    }
     /* fetch value */
     mysqli_stmt_fetch($stmt);
 
@@ -99,8 +109,10 @@ printf("%d Row inserted.\n", mysqli_stmt_affected_rows($stmt));
 }
 
 
+
+//mysqli_query($link, $query);
 echo "<font size = 4 color = red>".mysqli_error($link)."</font>";
-if (mysqli_affected_rows($link) == -1)
-echo "<font size = 4  color = red><b><b>insert into transactions2 NOT successful!</b></font><br>$query<br>";
-else
-echo "<font size = 4>insert success! </font><br>";
+//if (mysqli_affected_rows($link) == -1)
+//echo "<font size = 4  color = red><b><b>insert into transactions2 NOT successful!</b></font><br>$query<br>";
+//else
+//echo "<font size = 4>insert success! </font><br>";
