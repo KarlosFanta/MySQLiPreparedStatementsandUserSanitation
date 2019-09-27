6d03693bc01bb998d65e5d79be5cd14bbb488368
KarlosFanta <leeuwenpatrol@gmail.com>
2019-09-27 14:45:17 +0200

    Update webform1.php

--- webform1.php
+++ webform1.php
@@ -1,9 +1,9 @@
 <!-- Table structure:  NB I Have to add primary key!
  
-        CREATE TABLE `transactions` (
+        CREATE TABLE IF NOT EXISTS `transactions2` (
   `TransNo` int(11) NOT NULL,
   `CustNo` int(11) DEFAULT NULL,
-  `TransDate` date NOT NULL DEFAULT '0000-00-00',
+  `TransDate` date NOT NULL,
   `AmtPaid` float DEFAULT NULL,
   `Notes` varchar(500) DEFAULT NULL,
   `TMethod` varchar(30) DEFAULT NULL,
@@ -11,7 +11,7 @@
   `InvNoAincl` float DEFAULT NULL,
   `InvNoB` varchar(11) DEFAULT NULL,
   `InvNoBincl` float DEFAULT NULL,
-   PRIMARY KEY (`TransNo`),
+  PRIMARY KEY (`TransNo`),
   UNIQUE KEY `TransNo` (`TransNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
  -->
@@ -24,21 +24,25 @@ Here we can later add different types of inputs such as buttons, dropdowns, radi
 <?
 echo "<tr><th>TransNo</th>";
 echo "<th>CustNo</th>";
-echo "<th>TransDate</th>";
+echo "<th>TransDate<br>DD/MM/YYYY</th>";
 echo "<th>AmtPaid</th>";
+echo "<th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>";
 echo "<th>Payment Method</th>";
 echo "</tr>\n";
 ?>
                 <tr>
-                        <th><input type="text" size="2"  id="TransNo"  name="TransNo" value="" required />
+                        <th><input type="text" size="2"  id="TransNo"  name="TransNo" value="1" required />
                 </th>
-                        <th><input type="text" size="2"  id="CustNo"  name="CustNo" value="" required  />
+                        <th><input type="text" size="2"  id="CustNo"  name="CustNo" value="3" required  />
                 </th>
                 <th>   
-                        <input  id="TransDate" size="10" name="TransDate"  value="" required  >
+                        <input  id="TransDate" size="10" name="TransDate"  value="30/09/2019" required  >
                 </th>
                 <th>
-                        <input type="text"  size="5" id="AmtPaid"  name="AmtPaid" value="" />
+                        $<input type="text"  size="5" id="AmtPaid"  name="AmtPaid" value="1112.44" />
+                </th>
+                <th>
+                        $<input type="text"  size="20" id="Notes" name="Notes" value="My favourite customer" />
                 </th>
                 <th>
                         <select name="TMethod"  id="TMethod"  >
@@ -53,8 +57,5 @@ echo "</tr>\n";
                        
                 </th>
                 </tr>
-                </table>
- 
- 
-
-
+                </table><br>
+ <input type="submit"  size="5" value="submit" />
